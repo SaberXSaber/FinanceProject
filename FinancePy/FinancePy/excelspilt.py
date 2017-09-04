7b1d1405be018a5ad7155cf6a12a8248a5386b50
guoshubo <826944396@qq.com>
2017-09-04 17:35:51 +0800

    execl分割修正

--- FinancePy/FinancePy/excelspilt.py
+++ FinancePy/FinancePy/excelspilt.py
@@ -27,6 +27,8 @@ ncols = table.ncols
 sheets = nrows // limit
 print("sheets",sheets)
 
+print(table.row_values(0))
+
 # print str(nrows) + '   ' + str(ncols)
 
 # print table.cell(nrows - 1, ncols - 1).value
@@ -45,16 +47,24 @@ for i in range(0, sheets+1):
     workbook.save(savebook+"_"+str(i)+".xls")
     '''
     worksheet = workbook.add_sheet(str(i))
+
+    #添加表头
+    row_content = table.row_values(0)
+    for col in range(0, ncols):
+        worksheet.write(0, col, row_content[col])
+
     if i==sheets:
-        for row in range(0, nrows % limit):
+
+
+
+        for row in range(1, nrows % limit):
             row_content = table.row_values(row + (i * limit))
-            print(row + (i * limit))
+            print(table.row_values(row + (i * limit)))
             for col in range(0, ncols):
                 worksheet.write(row, col, row_content[col])
     else:
-        for row in range(0, limit):
+        for row in range(1, limit+1):
             row_content = table.row_values(row + (i * limit))
-            print(row + (i * limit))
             for col in range(0, ncols):
                worksheet.write(row, col, row_content[col])
     workbook.save(savebook+"_"+str(i)+".xls")
